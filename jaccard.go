00b8f669f71775de1a1ff9989850bacaa1428bf0
Ben Hawker <ben.c.hawker@gmail.com>
2019-11-25 17:16:56 +0000

    Jaccard

--- jaccard.go
+++ jaccard.go
@@ -6,23 +6,32 @@ package similarity
 
 // Suppose you want to find Jaccard similarity between two sets A and B
 // it is the ration of cardinality of A ∩ B and A ∪ B
-// func JaccardCoefficient(coords1, coords2 []float64) (float64, error) {
-// 	var intersection []float64
-// 	var union []float64
+func JaccardCoefficient(coords1, coords2 []float64) (float64, error) {
+	var intersection []float64
+	var union []float64
 
-// 	for index, coord := range coords1 {
-// 		if contains(coords2, coord) && !contains(intersection, coord) {
-// 			intersection = append(intersection, coord)
-// 		}
+	for _, coord := range coords1 {
+		if contains(coords2, coord) && !contains(intersection, coord) {
+			intersection = append(intersection, coord)
+		}
 
-// 		union = append(union, coord)
-// 	}
+		union = append(union, coord)
+	}
 
-// 	for index, coord := range coords2 {
-// 		if !contains(intersection, coord) {
-// 			union = append(union, coord)
-// 		}
-// 	}
+	for _, coord := range coords2 {
+		if !contains(intersection, coord) {
+			union = append(union, coord)
+		}
+	}
 
-// 	return len(intersection) / len(union), nil
-// }
+	return float64(len(intersection)) / float64(len(union)), nil
+}
+
+func contains(s []float64, e float64) bool {
+    for _, a := range s {
+        if a == e {
+            return true
+        }
+    }
+    return false
+}
--- jaccard_test.go
+++ jaccard_test.go
@@ -5,27 +5,50 @@ import (
 )
 
 func TestJaccardCoefficient(t *testing.T) {
-	// Same coords
-	a := []float64{1, 1}
-	b := []float64{1, 1}
+	a := []float64{1, 2}
+	b := []float64{1, 3, 4}
+	c := []float64{3, 4}
 	
-	distance, err := JaccardCoefficient(a, b)
+	distance, err := JaccardCoefficient(a, a)
 	if err != nil {
 		t.Error(err)
 	}
-	if almostEqual(distance, 1) != true {
-		t.Error("Expected distance of 1, got instead ", distance)
+	if almostEqual(distance, 1.00) != true {
+		t.Error("Expected distance of 1.00, got instead ", distance)
 	}
-
-	// Different coords with 2 dimensions
-	a = []float64{1, 2}
-	b = []float64{2, 3}
 
 	distance, err = JaccardCoefficient(a, b)
 	if err != nil {
 		t.Error(err)
 	}
-	if almostEqual(distance, 0.992277877) != true {
-		t.Error("Expected distance of 0.992277877, got instead ", distance)
+	if almostEqual(distance, 0.25) != true {
+		t.Error("Expected distance of 0.25, got instead ", distance)
+	}
+
+	distance, err = JaccardCoefficient(a, c)
+	if err != nil {
+		t.Error(err)
+	}
+	if almostEqual(distance, 0.00) != true {
+		t.Error("Expected distance of 0.00, got instead ", distance)
+	}
+
+	distance, err = JaccardCoefficient(b, c)
+	if err != nil {
+		t.Error(err)
+	}
+	if almostEqual(distance,  0.6666666666666666) != true {
+		t.Error("Expected distance of 0.6666666666666666, got instead ", distance)
+	}
+
+	a = []float64{1, 2, 3, 4}
+	b = []float64{1, 3, 4}
+
+	distance, err = JaccardCoefficient(a, b)
+	if err != nil {
+		t.Error(err)
+	}
+	if almostEqual(distance, 0.75) != true {
+		t.Error("Expected distance of 0.75, got instead ", distance)
 	}
 }
